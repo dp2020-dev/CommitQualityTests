c63abf51b74a90f1b4770396f50f05c2f6bc726a
danielphillips <ebost2004@yahoo.co.uk>
2023-04-24 19:30:58 +0100

    Initial commit

---
+++ src/test/java/LandingPageTests.java
@@ -0,0 +1,46 @@
+import net.bytebuddy.matcher.ElementMatcher;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import static org.junit.Assert.assertEquals;
+
+public class LandingPageTests {
+
+    private static WebDriver driver;
+
+    @BeforeClass
+    public static void setUp() {
+        // Set the path to the ChromeDriver executable (if necessary)
+        System.setProperty("webdriver.chrome.driver", "/opt/homebrew/bin/chromedriver");
+
+
+        // Create a new ChromeDriver instance
+        ChromeOptions options = new ChromeOptions();
+        driver = new ChromeDriver(options);
+        options.setHeadless(true);
+
+        driver.get("https://commitquality.com/");
+    }
+
+    @Test
+    public void testLandingPage() {
+        String expectedTitle = "CommitQuality - Test Automation Demo";
+
+        // Get the actual title of the webpage
+        String actualTitle = driver.getTitle();
+
+        // Verify that the actual title matches the expected title
+        assertEquals(expectedTitle, actualTitle);
+    }
+
+    // Close the browser
+    @AfterClass
+    public static void tearDown() {
+        driver.quit();
+    }
+
+}
