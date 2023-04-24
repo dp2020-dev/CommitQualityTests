ac8068759cb7d3033dffc85dc6bca0bdb5b9044b
danielphillips <ebost2004@yahoo.co.uk>
2023-04-24 20:40:22 +0100

    Add checkTableRowCOunt test to count rows in table.

--- src/test/java/LandingPageTests.java
+++ src/test/java/LandingPageTests.java
@@ -1,15 +1,22 @@
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
 public class LandingPageTests {
 
     private static WebDriver driver;
+    WebDriverWait wait = new WebDriverWait(driver, 10);
 
     @BeforeClass
     public static void setUp() {
@@ -35,6 +42,21 @@ public class LandingPageTests {
         // Verify that the actual title matches the expected title
         assertEquals(expectedTitle, actualTitle);
     }
+
+    @Test
+    public void checkTableRowCount() {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("product-list-table")));
+        // Select the table element using its class name
+        WebElement table = driver.findElement(By.className("product-list-table"));
+        List<WebElement> rows = table.findElements(By.tagName("tr"));
+        int rowCount = rows.size();
+        assertEquals(rowCount,11);
+    }
+
+    //verify page objects, create separate objects class
+    //test filter
+    //test reset
+    //test add a product
 
     // Close the browser
     @AfterClass
